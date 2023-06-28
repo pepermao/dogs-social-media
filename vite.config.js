4c64edf5202a9c41d226b2b3325c8fb331c98d35
pepermao <joaopedrobatistasantos501@gmail.com>
2023-06-28 21:40:06 +0200

    config deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/dogs-social-media",
   build: {
     rollupOptions: {
       outDir: '../build'
